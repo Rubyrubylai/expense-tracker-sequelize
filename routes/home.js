d7d91d36b585987fa18114520b81583ad31fb65a
Ruby Li <r844312@gmail.com>
2020-08-09 00:56:33 +0800

    Add month filter in home.js

--- routes/home.js
+++ routes/home.js
@@ -15,12 +15,22 @@ router.get('/', auth, (req, res) => {
     })
     .then(incomes => {
         let incomeAmount = 0
-       
-        //篩選為今天日期
-        incomes = incomes.filter(income => {
-            return income.date.toISOString().slice(0, 10) === new Date().toISOString().slice(0, 10)
-        })
+        console.log(new Date().toLocaleString('zh-TW', {timeZone: 'Asia/Taipei'}).slice(0, 10))
 
+        //篩選月份
+        if (req.query.month) {
+            incomes = incomes.filter(income => {
+                return income.date.getMonth() === req.query.month-1
+            })
+        }
+        else {
+            //篩選為今天日期
+            incomes = incomes.filter(income => {
+                console.log(income.date.toLocaleString('zh-TW', {timeZone: 'Asia/Taipei'}).slice(0, 10))
+                return income.date.toLocaleString().slice(0, 9) === new Date().toLocaleString('zh-TW', {timeZone: 'Asia/Taipei'}).slice(0, 9)
+            })
+        }
+        
         incomes.forEach(income => {
             //日期格式
             income.date = income.date.toISOString().slice(0,10)
@@ -36,19 +46,34 @@ router.get('/', auth, (req, res) => {
         .then(records => {
             let expenseAmount = 0
 
-             //篩選為今天日期
-             records = records.filter(record => {
-                return record.date.toISOString().slice(0, 10) === new Date().toISOString().slice(0, 10)
-            })
+            //篩選月份
+            if (req.query.month) {
+                records = records.filter(record => {
+                    return record.date.getMonth() === req.query.month-1
+                })
+            }
+            else {
+                //篩選為今天日期
+                records = records.filter(record => {
+                    return record.date.toISOString().slice(0, 10) === new Date().toISOString().slice(0, 10)
+                })
+            }
 
             records.forEach(record => {
                 record.date = record.date.toISOString().slice(0,10)
                 expenseAmount += record.amount
             })
 
+            //月份
+            let month = []
+            for (i=1; i<=12 ; i++) {
+                month.push(i)
+            }
+
+            //總金額
             let totalAmount = incomeAmount - expenseAmount
             
-            return res.render('index', { records, incomes, incomeAmount, expenseAmount, totalAmount })
+            return res.render('index', { records, incomes, incomeAmount, expenseAmount, totalAmount, month })
         })
     })
     .catch(err => console.error(err))   
