585498ce386225094e97dd3a15fca1511d4f5688
Ruby Li <r844312@gmail.com>
2020-11-10 00:44:03 +0800

    Update the function of merging the records that have same category in pie chart

--- routes/pieChart.js
+++ routes/pieChart.js
@@ -53,11 +53,14 @@ function show(deposit, deduct, records, req, res) {
   }
   else {
     //預設為目前年月份
+    records = records.filter(records => {
+      return Number(records.date.getMonth()) === Number(new Date().getMonth())
+    })
     monthYear = dateAfter.monthYear(new Date())
   }
 
+  //顯示收入和支出總額
   records.forEach((record, index) => {    
-    //總額
     if (record.balance === 'deposit') {
         depositAmount += record.amount
     } 
@@ -74,23 +77,36 @@ function show(deposit, deduct, records, req, res) {
     records = records.filter(record => record.balance === 'deduct')
   }
 
-  //將相同分類的收入或支出合併
+  //計算收入或支出總額，用來做百分比
   let totalAmount = 0
   records.forEach((record, i) => {
     totalAmount += Number(record.amount)  
-    if (i > 1) {
-      if (record.category === records[i-1].category) {
-        records[i-1].amount += record.amount
-        records.splice(i, 1)
-      }
-    }   
   })
-  records = records.map(record => ({
-    ...record,
-    percentage: Math.round((record.amount/totalAmount)*100)
-  }))
 
-  return res.render('pieChart', { deposit, deduct, records, totalAmount, depositAmount, deductAmount, monthYear })
+  //將相同分類的收入或支出合併
+  let tempArr = []
+  let endRecord = []
+  for (let i=0; i<records.length; i++) {
+    if (tempArr.indexOf(records[i].category) === -1) {
+      endRecord.push({
+        category: records[i].category,
+        amount: records[i].amount,
+        percentage: Math.round((records[i].amount/totalAmount)*100)
+      })
+      tempArr.push(records[i].category)
+    }
+    else {
+      for (let j=0; j<endRecord.length; j++) {
+        if (endRecord[j].category === records[i].category) {
+          endRecord[j].amount += records[i].amount
+          endRecord[j].percentage += Math.round((records[i].amount/totalAmount)*100)
+          break
+        }
+      }
+    }
+  }
+
+  return res.render('pieChart', { deposit, deduct, endRecord, totalAmount, depositAmount, deductAmount, monthYear })
 }
 
 module.exports = router
